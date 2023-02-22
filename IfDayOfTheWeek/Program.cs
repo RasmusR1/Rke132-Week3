e8037e923d2a2970a7b511957c4066b9e8e6e606
RasmusR <rasmus.raadik@tktk.ee>
2023-02-22 17:39:15 +0200

    add week3 code

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int WeekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+Console.WriteLine(WeekDay);
+
+if (WeekDay == 0)
+{
+    Console.WriteLine("On Pühapäev!");
+}
+else if (WeekDay == 1)
+{
+    Console.WriteLine("On Esmaspäev!");
+}
+else if (WeekDay == 2)
+{
+    Console.WriteLine("On Teisipäev!");
+}
+else if (WeekDay == 3)
+{
+    Console.WriteLine("On Kolmapäev!");
+}
+else if (WeekDay == 4)
+{
+    Console.WriteLine("On Neljapäev!");
+}
+else if (WeekDay == 5)
+{
+    Console.WriteLine("On Reede!");
+}
+else 
+{
+    Console.WriteLine("On Laupäev!");
+}
+
+Console.WriteLine("Hyva Päeva");
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+int WeekDay = 10; //Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+switch (WeekDay)
+{
+    case 0:
+        Console.WriteLine("On Pühapäev");
+        break;
+    case 1:
+        Console.WriteLine("On Esmaspäev");
+        break;
+    case 2:
+        Console.WriteLine("On Teisipäev");
+        break;
+    case 3:
+        Console.WriteLine("On Kolmapäev");
+        break;
+    case 4:
+        Console.WriteLine("On Neljapäev"); 
+        break;
+    case 5:
+        Console.WriteLine("On Reede");
+        break;
+    case 6:
+        Console.WriteLine("On Laupäev");
+    break;
+    default:
+        Console.WriteLine("Kalender valet pidi");
+    break;
+}
+
+Console.WriteLine("Hüva päeva");
